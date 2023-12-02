68aaae881c7349403a167f8e68ef721e43d66be0
MiniDigger | Martin <admin@benndorf.dev>
2023-12-02 13:29:05 +0100

    Initial commit

---
+++ src/main/java/org/example/springjdbirepositoryissue/TestApplication.java
@@ -0,0 +1,14 @@
+package org.example.springjdbirepositoryissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableJdbiRepositorie
+@SpringBootApplication
+public class SpringJdbirepositoryIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJdbirepositoryIssueApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/springjdbirepositoryissue/TestRepository.java
@@ -0,0 +1,2 @@
+package org.example.springjdbirepositoryissue;public class TestRepository {
+}
